fe7937376a26ee66a3f7e089c8b419dcfb945bc3
Nikit-370 <81354106+Nikit-370@users.noreply.github.com>
2021-05-09 16:10:18 +0530

    Create clock12.js

---
+++ clock12.js
@@ -0,0 +1,25 @@
+function currentTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var midday = "AM";
+    midday = (hour >= 12) ? "PM" : "AM";
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + midday;
+    var t = setTimeout(currentTime, 1000);
+}
+
+function updateTime(k) {
+    if (k < 10) {
+        return "0" + k;
+    }
+    else {
+        return k;
+    }
+}
+
+currentTime();
